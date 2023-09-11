13204d73e34e9a8f3dbed93d2ea39b0037ff4f01
NaviProtocolL <lin@naviprotocol.io>
2023-09-11 16:05:12 +0800

    update

--- main.ts
+++ main.ts
@@ -9,13 +9,6 @@ export class SuiTools {
 
     async main() {
         console.log(this.sui.address());
-        // await this.oracleOption();
-
-        // await this.storageManager.initAllReserve();
-
-        // await this.sui.getObject('0xd5ec9d12f89a985b9e430ca8ace8183a76f3c52a0c2a09a67db42ebf60613fce');
-        // await this.storageManager.addIncentivePool();
-        // await this.oracleManager.updatePrice();
     }
 }
 
